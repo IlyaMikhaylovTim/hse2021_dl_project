7aadb5ecebde4aeb4f04b5fd1726dff877978cbd
gal-dmitry <galimzyanov1996@gmail.com>
2021-06-11 19:42:40 +0000

    Hello, world! 11

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .mlp import MLP
 from .nac import NeuralAccumulatorCell, NAC
+from .complex_nac import Complex_NeuralAccumulatorCell, Complex_NAC
 from .nalu import NeuralArithmeticLogicUnitCell, NALU
